b515f136f39676a40cfdcd51acc22bb37d6ff03a
ashishpayasi25 <44580452+ashishpayasi25@users.noreply.github.com>
2018-10-29 21:55:24 +0530

    Update sum_digits.py

--- sum_digits.py
+++ sum_digits.py
@@ -1,16 +1,8 @@
 #!/usr/bin/env python
-
-from __future__ import print_function
-try:
-    import readline
-except ImportError:
-    pass
-
-def sum_of_digits(digit_string):
-    """Return the sum of the digits in the digit string."""
-    return -1
-
-if __name__ == '__main__':
-    digit_string = raw_input('Sum digits of this number: ')
-    sum = sum_of_digits(digit_string)
-    print('Sum of digits: {0}'.format(sum))
+num=input("Enter a number)
+sum=0     
+while(num!=0):
+          tmp=num%10
+          num=num/10
+          sum=sum+tmp
+ print("sum is :" +sum)
